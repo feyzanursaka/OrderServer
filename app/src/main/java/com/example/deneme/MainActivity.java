31536494780fa3665650b5e97add54550bc8b7ee
feyzanursaka <feyzanursaka1996@gmail.com>
2022-08-09 10:04:43 +0300

    comment line

--- app/src/main/java/com/example/deneme/MainActivity.java
+++ app/src/main/java/com/example/deneme/MainActivity.java
@@ -1,124 +1,60 @@
 package com.example.deneme;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.HorizontalScrollView;
-import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView t1;
-    String mesaj="";
-    Typeface tf1;
-
-
+    TextView t1;//gelen mesajların gösterildiği textview tanımı
+    String mesaj = "";//gelen mesajın tutuldugu string tanımı
+    Typeface tf1;//yazı tipi uygulamak için typeface tanımlama
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1 = (TextView) findViewById(R.id.textView);//gelen mesajı gösteren textview'i id den bulup t1'e atama
+        t1.setMovementMethod(new ScrollingMovementMethod());//textview için sayfayı kaydırma özelliği ekleme
+        tf1 = Typeface.createFromAsset(getAssets(), "font/Cantata.ttf");//yazı tipi kullanma
+        t1.setTypeface(tf1);//textviewde gösterilen yazıya cantata yazıtipi uygulama
 
-        //e1=(EditText) findViewById(R.id.editTextTextPersonName2);
-
-        t1=(TextView) findViewById(R.id.textView);
-        t1.setMovementMethod(new ScrollingMovementMethod());
-        tf1= Typeface.createFromAsset(getAssets(),"font/Cantata.ttf");
-        t1.setTypeface(tf1);
-
-
-        Thread myThread=new Thread(new MyServerThread());
+        Thread myThread = new Thread(new MyServerThread());
         myThread.start();
-
-
-
     }
 
-    class MyServerThread implements Runnable
-    {
-        Socket s;
-        ServerSocket ss;
-        InputStreamReader isr;
-        BufferedReader br;
-        Handler h = new Handler();
-        String message;
-
-         Thread thread;
-         ServerSocket serverSocket;
-         Socket socket;
-         DataInputStream dataInputStream;
-         DataOutputStream dataOutputStream;
-
-
-
-
-
-
+    class MyServerThread implements Runnable {
+        ServerSocket serverSocket;
+        Socket socket;
+        DataInputStream dataInputStream;
         @Override
         public void run() {
-            try
-            {
-                serverSocket = new ServerSocket( 8080 );
-            }
-            catch ( IOException e )
-            {
-                System.out.println( "failed to start server socket" );
+            try {
+                serverSocket = new ServerSocket(8080);//8080 portunu kullanmak için belirtme
+                System.out.println("waiting for connections...");
+            } catch (IOException e) {
+                System.out.println("failed to start server socket");//porta bağlanamazsa hata gösterme
                 e.printStackTrace();
             }
-
-            // wait for a connection
-            System.out.println( "waiting for connections..." );
-
-
-            while ( true )
-            {
-                try
-                {
-                    socket = serverSocket.accept();
-                }
-                catch ( IOException e )
-                {
-                    System.out.println( "failed to accept" );
-                    e.printStackTrace();
-                }
-                System.out.println( "client connected" );
-                try
-                {
-                    dataInputStream = new DataInputStream( new BufferedInputStream( socket.getInputStream() ) );
-                    mesaj += dataInputStream.readUTF()+"\n\n";
-                    t1.setText(mesaj);
-
-
-
-
-                }
-                catch ( IOException e )
-                {
+            while (true) {
+                try {
+                    socket = serverSocket.accept();//server başladıktan sonra bağlanan clientı kabul etme
+                    System.out.println("client connected");
+                    dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));//geken mesajı datainputstream ile alma
+                    mesaj += dataInputStream.readUTF() + "\n\n";//readUTF ile string mesajı datainputstreamden okuma
+                    t1.setText(mesaj);//gelen mesajı textviewde gösterme
+                } catch (IOException e) {
+                    System.out.println("failed to accept");//bağlantı kurulamadıysa hata göster
                     e.printStackTrace();
                     break;
                 }
             }
-
-            System.out.println( "server thread stopped" );
-                }
-            }
-
-
+        }
+    }
 }
